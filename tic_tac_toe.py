d8550050cfe888b958bc84cc057ec2fa79b7a05f
ahpmh <alwayshappypeople@gmail.com>
2021-10-11 14:12:33 +0200

    probamodositas

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,7 +8,7 @@ def get_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
     row, col = 0, 0
     return row, col
-
+#modositas proba
 
 def get_ai_move(board, player):
     """Returns the coordinates of a valid move for player on board."""
